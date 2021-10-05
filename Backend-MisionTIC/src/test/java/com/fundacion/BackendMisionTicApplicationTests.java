312940fdebd6931d5f1d547f38dd7f24956fe4b1
jhonson <jhonson.abella@o365.unab.edu.co>
2021-10-04 20:17:16 -0500

    creacion del proyecto backend

---
+++ Backend-MisionTIC/src/test/java/com/fundacion/BackendMisionTicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMisionTicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
